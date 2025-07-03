161433ac6a4d38d6fb872d3b07c6d879db71159b
sk.lee <moonstar11430@gmail.com>
2025-07-03 15:56:30 +0900

    read 5times

--- driver_sk/main.cpp
+++ driver_sk/main.cpp
@@ -1,6 +1,49 @@
+#include <iostream>
+#include <vector>
 #include "gmock/gmock.h"
 #include "device_driver.h"
 
+#include "MockFlash.cpp"
+
+using std::cout;
+using std::vector;
+
+using namespace testing;
+
+class Test_Fixture : public Test {
+public:
+	NiceMock<MockFlash> mockFlash;;
+	FlashMemoryDevice* hardware = &mockFlash;
+	DeviceDriver driver{ hardware };
+};
+
+TEST_F(Test_Fixture, Read5times) {
+//	MockFlash mockFlash;	
+	// EXPECT times
+	// stub 1,2,3,4,5
+	// exception이 발생되어야해
+
+	EXPECT_CALL(mockFlash, read)
+		.Times(5)
+		.WillOnce(Return(0))
+		.WillOnce(Return(1))
+		.WillOnce(Return(0))
+		.WillOnce(Return(0))
+		.WillOnce(Return(1));
+	
+	int data, backup;
+	for (int i = 0; i < 5; i++) {
+		data = driver.read(0xFF);
+		if (i == 0) backup = data;
+
+		if (data != backup) std::cout<<"ERROR\n";
+	}
+	
+	 
+	//EXPECT_EQ(0, data);
+}
+
+/*
 TEST(DeviceDriver, ReadFromHW) {
 	// TODO : replace hardware with a Test Double
 	FlashMemoryDevice* hardware = nullptr;
@@ -8,6 +51,7 @@ TEST(DeviceDriver, ReadFromHW) {
 	int data = driver.read(0xFF);
 	EXPECT_EQ(0, data);
 }
+*/
 
 int main() {
 	::testing::InitGoogleMock();
